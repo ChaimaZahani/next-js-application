4918150bf3c8240fbf70dd0ac313af92bd407f67
Chaima Zahani <chaima.zahani@enis.tn>
2023-06-29 13:00:58 +0100

    commit

--- components/CustomFilter.tsx
+++ components/CustomFilter.tsx
@@ -7,22 +7,16 @@ import { Listbox, Transition } from "@headlessui/react";
 import { CustomFilterProps } from "@/types";
 import { updateSearchParams } from "@/utils";
 
-const CustomFilter = ({ title, options }: CustomFilterProps) => {
-  const router = useRouter();
-  const [selected, setSelected] = useState(options[0]); 
+const CustomFilter = ({ title, options, setFilter }: CustomFilterProps) => {
+  const [selected, setSelected] = useState(options[0]);
 
-  const handleUpdateParams = (e: { title: string; value: string }) => {
-    const newPathName = updateSearchParams(title, e.value.toLowerCase());
-
-    router.push(newPathName);
-  };
   return (
     <div className="w-fit">
       <Listbox
         value={selected}
         onChange={(e) => {
           setSelected(e);
-          handleUpdateParams(e);
+          setFilter(e.value);
         }}
       >
         <div className="relative w-fit z-10">
@@ -44,15 +38,15 @@ const CustomFilter = ({ title, options }: CustomFilterProps) => {
           >
             <Listbox.Options className="custom-filter__options">
               {options.map((option) => (
-                <Listbox.Option 
-                key={option.title} 
-                value={option}
-                className={({ active }) =>
+                <Listbox.Option
+                  key={option.title}
+                  value={option}
+                  className={({ active }) =>
                     `relative cursor-default select-none py-2 px-4 ${
                       active ? "bg-primary-blue text-white" : "text-gray-900"
                     }`
                   }
-                  >
+                >
                   {({ selected }) => (
                     <>
                       <span
--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -5,8 +5,8 @@ import { manufacturers } from "@/constants";
 import { Combobox, Transition } from "@headlessui/react";
 import Image from "next/image";
 const SearchManufacturer = ({
-  manufacturer,
-  setManuFacturer,
+  selected,
+  setSelected,
 }: SearchManufacturerProps) => {
   const [Query, setQuery] = useState("");
 
@@ -21,7 +21,7 @@ const SearchManufacturer = ({
         );
   return (
     <div className="search-manufacturer">
-      <Combobox value={manufacturer} onChange={setManuFacturer}>
+      <Combobox value={selected} onChange={setSelected}>
         <div className="relative w-full">
           <Combobox.Button className="absolute top-[14px]">
             <Image
